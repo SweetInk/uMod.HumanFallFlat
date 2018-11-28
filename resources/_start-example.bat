5db558e39b3b2c5b78a504478cd4fc35ee9022d9
Luke Spragg <luke@spragg.me>
2018-11-28 02:36:18 -0500

    Updated project file and example .bat

--- resources/_start-example.bat
+++ resources/_start-example.bat
@@ -3,8 +3,8 @@ cls
 :start
 echo Starting server...
 
-Human.exe -batchmode -nographics -dedicated -logfile output_log.txt -servername "My uMod Server" -maxplayers 10
-@rem Human.exe -logfile output_log.txt -servername "My uMod Server" -maxplayers 10
+@rem Human.exe -batchmode -nographics -dedicated -logfile output_log.txt -servername "My uMod Server" -maxplayers 10
+Human.exe -logfile output_log.txt -servername "My uMod Server" -maxplayers 10
 
 echo.
 echo Restarting server...
